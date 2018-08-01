860b131012a69c07bffb356484b09334f64ee32b
Kev Thompson <thompson.kev@googlemail.com>
2018-08-01 16:10:23 +0100

    Created a service for the movie converter but there is a memory issue

--- app/src/main/java/grungesoft/com/stopmotioncamera/MovieConverterActivity.java
+++ app/src/main/java/grungesoft/com/stopmotioncamera/MovieConverterActivity.java
@@ -1,29 +1,15 @@
 package grungesoft.com.stopmotioncamera;
 
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Environment;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ActionMenuView;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.ByteArrayOutputStream;
-
-import grungesoft.com.stopmotioncamera.Utilities.MMediaMuxer;
-import grungesoft.com.stopmotioncamera.Utilities.SavingPicture;
+import grungesoft.com.stopmotioncamera.Services.MovieConverterService;
 import grungesoft.com.stopmotioncamera.Utilities.Util;
-
-import static grungesoft.com.stopmotioncamera.MainActivity.context;
 
 public class MovieConverterActivity extends AppCompatActivity {
 
@@ -38,8 +24,6 @@ public class MovieConverterActivity extends AppCompatActivity {
     }
 
     TextView feedbackText;
-    Handler handler = new Handler();
-    MMediaMuxer myMuxer = new MMediaMuxer();
 
     /**
      *
@@ -50,86 +34,26 @@ public class MovieConverterActivity extends AppCompatActivity {
         Button startButton = (Button)findViewById(R.id.convert_movie_button);
         feedbackText = (TextView)findViewById(R.id.convert_movie_feedback_text);
 
-        setFeedback("test");
-
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Util.log(LOG_TAG,"Convert Movie Clicked");
-
-
-create a service!
-                Thread t = new Thread() {
-
-                    public void run() {
-                        handler.post(new Runnable() {
-
-                            public void run() {
-                                setFeedback("test loop");
-
-
-                                int index;
-                                for(index = 1; index <= 15; index++) {
-                                    setFeedback("Getting Frame ( " + index + ") ");
-                                    byte[] bytes =getFrame(index);
-
-                                    setFeedback("Adding Frame ( " + index + ") ");
-  //                                  myMuxer.AddFrame(bytes);
-                                }
-                                setFeedback("Done - Creating video");
-//                                myMuxer.CreateVideo();
-
-                            }
-
-                        });
-                    }
-
-                };
-                t.start();
-
-
-                setFeedback("Starting");
-
-
+                startServiceNow();
             }
         });
     }
 
+
     /**
      *
-     * @param str
      */
-    private void setFeedback(final String str)
+    private void startServiceNow()
     {
-        Util.log(LOG_TAG,str);
+        Util.log(LOG_TAG,"startServiceNow - set intent");
+        Intent movieConverterService = new Intent(this, MovieConverterService.class);
 
-//        runOnUiThread(new Runnable()
-//        {
-//            @Override
-//            public void run()
-//            {
-                feedbackText.setText(str);
-//            }
-//        });
-
-    }
-
-    private byte[] getFrame(int index)
-    {
-        String path = SavingPicture.getInstance().getPictureFileDir().getPath();
-        String fileName = index + ".jpg";
-
-        String fullPath = path + "/" + fileName;
-
-        Bitmap bmp = BitmapFactory.decodeFile(fullPath);
-
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
-        byte[] byteArray = stream.toByteArray();
-        bmp.recycle();
-
-
-        return byteArray;
+        Util.log(LOG_TAG,"startServiceNow - start");
+        startService(movieConverterService);
 
     }
 }
---
+++ app/src/main/java/grungesoft/com/stopmotioncamera/Services/MovieConverterService.java
@@ -0,0 +1,67 @@
+package grungesoft.com.stopmotioncamera.Services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.ByteArrayOutputStream;
+
+import grungesoft.com.stopmotioncamera.Utilities.MMediaMuxer;
+import grungesoft.com.stopmotioncamera.Utilities.SavingPicture;
+import grungesoft.com.stopmotioncamera.Utilities.Util;
+
+public class MovieConverterService extends IntentService {
+
+    MMediaMuxer myMuxer_ = new MMediaMuxer();
+
+    /**
+     * Constructor
+     */
+    public MovieConverterService()
+    {
+        super("MovieConverterService");
+    }
+
+    /**
+     * Thread Where we make the movie!
+     * @param workIntent
+     */
+    protected void onHandleIntent(Intent workIntent)
+    {
+        Util.log("MovieConverter", "Starting MovieConversion:");
+
+
+        int index;
+        for(index = 1; index <= 15; index++) {
+            Util.log("MovieConverter", "Get Frame " + index);
+            byte[] bytes =getFrame(index);
+
+            Util.log("MovieConverter", "Add Frame " + index);
+            myMuxer_.AddFrame(bytes);
+        }
+        myMuxer_.CreateVideo();
+
+
+    }
+
+
+    private byte[] getFrame(int index)
+    {
+        String path = SavingPicture.getInstance().getPictureFileDir().getPath();
+        String fileName = index + ".jpg";
+
+        String fullPath = path + "/" + fileName;
+
+        Bitmap bmp = BitmapFactory.decodeFile(fullPath);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        bmp.recycle();
+
+
+        return byteArray;
+
+    }
+}
